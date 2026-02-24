1dd33e490b4d68b40f0699696181cedae14dae0b
Kaosovanna <kaosovanna.pas@gmail.com>
2026-02-24 12:09:51 +0100

    debug: update v.php to trace DB connection

--- v.php
+++ v.php
@@ -1,3 +1,15 @@
 <?php
-die("VER_777_STABLE");
+// v.php - DIAGNOSTIC ENTRY POINT
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "Checking DB connection...\n";
+require_once __DIR__ . '/noyau_backend/configuration/db.php';
+
+if (isset($pdo)) {
+    echo "SUCCESS: PDO is initialized.\n";
+}
+else {
+    echo "FAILURE: PDO is still null.\n";
+}
 ?>
