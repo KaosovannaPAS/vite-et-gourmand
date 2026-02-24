7df4d25c9fbaeb807bf33bea5ab33c1760363274
Kaosovanna <kaosovanna.pas@gmail.com>
2026-02-24 11:17:54 +0100

    fix(api): force fresh load with MenuV2 and update API require

--- noyau_backend/api/v1/menus.php
+++ noyau_backend/api/v1/menus.php
@@ -2,9 +2,9 @@
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
 
-require_once __DIR__ . '/../../models/Menu.php';
+require_once __DIR__ . '/../../models/MenuV2.php';
 
-$menuModel = new Menu();
+$menuModel = new MenuV2();
 $method = $_SERVER['REQUEST_METHOD'];
 
 if ($method === 'GET') {
--- noyau_backend/models/MenuV2.php
+++ noyau_backend/models/MenuV2.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../configuration/db.php';
 
-class Menu
+class MenuV2
 {
     private $pdo;
 
